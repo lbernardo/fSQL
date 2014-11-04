db0d4ae549c504b2a0ea47664449e88918f00dad
lbernardo <lucas080795@hotmail.com>
2014-11-04 14:18:23 -0200

    Conexão com banco de dados por meio de objetos

--- fSQL.php
+++ fSQL.php
@@ -3,12 +3,15 @@ class fSQL{
 	private $data = array();
 	var $con;
 	var $col = array();
+	
+	// Seta nova variavel
 	public function __set($name,$value){
 		
 		$this->data[$name] = $value;		
 
 	}
-
+	
+	// Pega nova variavel
 	public function __get($name)
     	{
         	if (array_key_exists($name, $this->data)) {
@@ -16,6 +19,7 @@ class fSQL{
         	}
 	}
 
+	// Inicia conexão
 	public function __construct($host,$user,$pass,$db,$type = 'mysql'){
 	
 		$this->con = new PDO("$type:host=$host;dbname=$db",$user,$pass);
@@ -23,6 +27,8 @@ class fSQL{
 	}
 
 
+	// Seleciona uma tabela
+	// fSQL_TABLE from(NOME_TABELA)
 	public function from($table){
 		return new fSQL_Table($table,$this->con);
 	}
@@ -30,64 +36,98 @@ class fSQL{
 
 }
 
+// Classe para controle da tabela
 class fSQL_Table extends fSQL{
 
+	// A tabela
 	var $table;
+	// SQL 
 	var $SQL;
+	// SQL RESOURCE
 	var $SQL_R;
+	// Consulta
 	var $CON;
+	// Consulta da tabela
 	var $CON_TABLE;
+	// Colunas
 	var $cols = array();
+	// Se deseja criar uma nova linha
 	var $create_new_row = false;
+	// WHERE
 	var $VAR_WHERE = '';
+	// Número de linhas
 	var $rowCount = 0;
+	// Ultimo id
 	var $lastId = null;
 
 	
 	
-	
+	// Metodo contrutor da tabela 
+		// (NOME_TABELA,RESOURCE de CONEXÃO PDO)
 	public function __construct($table,$resource){
+		// Marca tabela atual
 		$this->table = $table;
+		// Salva conexão
 		$this->CON = $resource;
+		// Realiza consulta das colunas da tabela
 		$consulta = $resource->prepare("desc $table");
+		// GEra
 		$consulta->execute();
-
+		// Verifica o nome das colunas executando um while
 		while($retorno = $consulta->fetch(PDO::FETCH_NUM)){
+			// Insere as colunas no metodo de coluna
 			array_push($this->cols,$retorno[0]);
+			// Cria colunas como objeto
 			$this->$retorno[0] = null;
+			// Cria nomes temporarios
 			$name = "f_".$retorno[0];
+			// Cria colunas com nome temporario
 			$this->$name = null; 
 			
 		}
 
-
+		// Gera SQL
 		$this->SQL = "SELECT `".implode("`,`",$this->cols)."` FROM $table ";
+		// Executa SQL
 		$this->exec();
 
 	}
 
 
+	// Executa WHERE
+	// VOID where(SQL_WHERE)
 	public function where($where){
-
+		// Muda SQL para SQL com WHERE
 		$this->SQL= " SELECT `".implode("`,`",$this->cols)."` FROM $this->table WHERE $where";
+		// Salva where em uma variavel
 		$this->VER_WHERE = $where;
+		// Executa consulta
 		$this->exec();
 
 	}
 
+	// Executa Consulta
+	// VOID exec(void)
 	private function exec(){
-		$this->CON_TABLE = $this->CON->prepare($this->SQL);
-		$this->CON_TABLE->execute();
+		// Consulta da tabela
+		$this->CON_TABLE = $this->CON->query($this->SQL);
+		// Salva o número de linhas encontradas
 		$this->rowCount = $this->CON_TABLE->rowCount();
 		
 	}
 
-
+	// Retorna valores salvando como objetos
+	// RESOURCE run(void)
 	public function run(){
+		// Consulta fetch
 		$fetch  = $this->CON_TABLE->fetch(PDO::FETCH_ASSOC);
+		// Se retornar algum valor
 		if($fetch!=false){
+			// Lê todos os nomes  das colunas
 			foreach($this->cols as $collum){
+				// Salva as colunas 
 				$this->$collum = $fetch[$collum];
+				// Salva a coluna temporaria
 				$j = "f_".$collum;
 				$this->$j = $fetch[$collum];
 			}
@@ -96,107 +136,104 @@ class fSQL_Table extends fSQL{
 	}
 
 
-
+	// Gera uma consulta SQL
+	// PDOStatement SQL(SQL)
 	public function SQL($sql){
 		return $this->CON->query($sql);
 	}
 
-
+	// Diz para classe que deseja criar uma nova linha na tabela
+	// VOID newRow(null)
 	public function newRow(){
+		// Confirma que é uma nova linha
 		$this->create_new_row = true;
+		// Lê todas as colunas
 		foreach($this->cols as $name){
+		      // Muda colunas temporarias para NULL (entendera mais na frente)
                        $f = "f_".$name;
-                       $this->$f = null;                
-                       
+                       $this->$f = null;                                       
 		}
 
 	}
 
+	// Salva Linha na tabela
+	// PDOStatement save(VOID)
 	public function save(){
 	
+		// Verifica se não esta inserindo uma nova linha, então esta atualizando a linha atual
 		if($this->create_new_row==false){
 			
 			$cl = array();
 			$wh = array();
+			// Lê todas as colunas
 			foreach($this->cols as $name){
 				$f = "f_".$name;
-				if($this->$name!=$this->$f){
+				// Se estiver diferente a coluna temporaria com a coluna verdadeira
+				if($this->$name!=$this->$f){ // Foi realiza edição nesta coluna
+					// Insere em array temporario edição
 					array_push($cl,"`$name`='".$this->$name."'");
 				}
+				// Adiciona where na consulta
 				array_push($wh,"`$name`='".$this->$f."'");
-			}
-			
-			
+			}		
 
+			//SQL RESOURCE (INSERT)
 			$this->SQL_R = "UPDATE $this->table SET ".implode(",",$cl)." WHERE ".implode(" AND ",$wh);
 		
-
-			
+	
 		
 
-		}else{
-			$cl = array();
+		}else{ // Esta inserindo uma nova linha
+			$cl = array(); // Coluna
+			// Lê todas as colunas
 	                foreach($this->cols as $name){
+				// Insere em um array de colunas
         	                array_push($cl,$this->$name);
                 	}
 
+			// SQK RESOURCE (INSERT)
 	                $this->SQL_R = "INSERT INTO $this->table (`".implode("`,`",$this->cols)."`) VALUES('".implode("','",$cl)."')";
 
 		}
+		// SQL temporario é o SQL RESOURCE (UPDATE/INSERT)
 		$sql_temp = $this->SQL_R;
-//		$this->SQL_R = null;
+		// Executa SQL temporario
 		$j = $this->CON->query($sql_temp);
-		if($this->create_new_row==true){
+		// Se foi marcado como nova linha
+		if($this->create_new_row==true){	
+			// Salva como linha antiga
 			$this->create_new_row = false;
+			// Retorna ultimo id
 			$this->lastId = $this->CON->lastInsertId();
 		}
 		
+		// Retorna PDOStatement
                 return $j;
 		
 
 	}
 
 
+	// Deleta linha atual
+	// PDOStatement deleteRow(VOID)
 	public function deleteRow(){
-		 $cl = array();	
+		 $cl = array();// Colunas
+ 		 // Lê todas as colunas		
 		 foreach($this->cols as $name){
+				// colunas temporarias
                                 $f = "f_".$name;
-                                array_push($cl,"`$name`='".$this->$name."'");
-                               
-                               
+				// Cria where
+                                array_push($cl,"`$name`='".$this->$name."'");                              
                  }
 
+		// SQL para deletar linha
 		$sql = "DELETE FROM $this->table where ".implode(" AND ",$cl);
                 
-		return $this->CON->query($sql);
-
-		
+		// Retorna PDOSatement
+		return $this->CON->query($sql);	
 
 	}
 
 
 }
-
-/*
-$fSQL = new fSQL("localhost","root","admin","intra");
-
-$table = $fSQL->from("lis_ref_emp_contatos");
-
-$table->where("id_ref_empcontatos>0");
-
-while($table->run()){
-	echo $table->id_contatos." | ".$table->id_empresa."\n";
-}
-
-$table->newRow();
-
-$table->id_contatos = rand(0,100);
-$table->id_empresa = rand(0,rand(1,30));
-$table->id_ref_empcontatos = null;
-
-if($table->save())
-	echo "OK";
-else
-	echo "Erro".$table->SQL_R;
-*/
 ?>
